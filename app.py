c18ec65401c20d25ba5d99cbd2b84dc04960ccad
mariannan17 <mcn332@nyu.edu>
2024-04-18 19:46:20 -0400

    changes to app.py

--- app.py
+++ app.py
@@ -7,198 +7,77 @@ import datetime
 
 from flask import Flask, render_template, request, redirect, url_for, make_response
 
-# import logging
-import sentry_sdk
-from sentry_sdk.integrations.flask import (
-    FlaskIntegration,
-)  # delete this if not using sentry.io
-
-# from markupsafe import escape
 import pymongo
 from pymongo.errors import ConnectionFailure
 from bson.objectid import ObjectId
 from dotenv import load_dotenv
 
-# load credentials and configuration options from .env file
-# if you do not yet have a file named .env, make one based on the template in env.example
-load_dotenv(override=True)  # take environment variables from .env.
+load_dotenv(override=True)  
 
-# initialize Sentry for help debugging... this requires an account on sentrio.io
-# you will need to set the SENTRY_DSN environment variable to the value provided by Sentry
-# delete this if not using sentry.io
-sentry_sdk.init(
-    dsn=os.getenv("SENTRY_DSN"),
-    # enable_tracing=True,
-    # Set traces_sample_rate to 1.0 to capture 100% of transactions for performance monitoring.
-    traces_sample_rate=1.0,
-    # Set profiles_sample_rate to 1.0 to profile 100% of sampled transactions.
-    # We recommend adjusting this value in production.
-    profiles_sample_rate=1.0,
-    integrations=[FlaskIntegration()],
-    traces_sample_rate=1.0,
-    send_default_pii=True,
-)
-
-# instantiate the app using sentry for debugging
 app = Flask(__name__)
 
-# # turn on debugging if in development mode
-# app.debug = True if os.getenv("FLASK_ENV", "development") == "development" else False
-
-# try to connect to the database, and quit if it doesn't work
 try:
     cxn = pymongo.MongoClient(os.getenv("MONGO_URI"))
-    db = cxn[os.getenv("MONGO_DBNAME")]  # store a reference to the selected database
+    db = cxn[os.getenv("MONGO_DBNAME")]
 
-    # verify the connection works by pinging the database
-    cxn.admin.command("ping")  # The ping command is cheap and does not require auth.
-    print(" * Connected to MongoDB!")  # if we get here, the connection worked!
+    cxn.admin.command("ping")  
+    print(" * Connected to MongoDB!")
 except ConnectionFailure as e:
-    # catch any database errors
-    # the ping command failed, so the connection is not available.
-    print(" * MongoDB connection error:", e)  # debug
-    sentry_sdk.capture_exception(e)  # send the error to sentry.io. delete if not using
-    sys.exit(1)  # this is a catastrophic error, so no reason to continue to live
-
-
-# set up the routes
-
+    print(" * MongoDB connection error:", e)  
+    sys.exit(1)  
 
 @app.route("/")
 def home():
-    """
-    Route for the home page.
-    Simply returns to the browser the content of the index.html file located in the templates folder.
-    """
-    return render_template("index.html")
+    hotels = db.hotels.find()
+    return render_template("index.html", hotels=hotels)
 
+@app.route("/book/<hotel_id>")
+def book(hotel_id):
+    hotel = db.hotels.find_one({"_id": ObjectId(hotel_id)})
+    return render_template("book.html", hotel=hotel)
 
-@app.route("/read")
-def read():
-    """
-    Route for GET requests to the read page.
-    Displays some information for the user with links to other pages.
-    """
-    docs = db.exampleapp.find({}).sort(
-        "created_at", -1
-    )  # sort in descending order of created_at timestamp
-    return render_template("read.html", docs=docs)  # render the read template
+@app.route("/check_in/<hotel_id>", methods=["POST"])
+def check_in(hotel_id):
+    name = request.form["name"]
+    email = request.form["email"]
+    check_in_date = datetime.datetime.strptime(request.form["check_in_date"], "%Y-%m-%d")
+    check_out_date = datetime.datetime.strptime(request.form["check_out_date"], "%Y-%m-%d")
 
-
-@app.route("/create")
-def create():
-    """
-    Route for GET requests to the create page.
-    Displays a form users can fill out to create a new document.
-    """
-    return render_template("create.html")  # render the create template
-
-
-@app.route("/create", methods=["POST"])
-def create_post():
-    """
-    Route for POST requests to the create page.
-    Accepts the form submission data for a new document and saves the document to the database.
-    """
-    name = request.form["fname"]
-    message = request.form["fmessage"]
-
-    # create a new document with the data the user entered
-    doc = {"name": name, "message": message, "created_at": datetime.datetime.utcnow()}
-    db.exampleapp.insert_one(doc)  # insert a new document
-
-    return redirect(
-        url_for("read")
-    )  # tell the browser to make a request for the /read route
-
-
-@app.route("/edit/<mongoid>")
-def edit(mongoid):
-    """
-    Route for GET requests to the edit page.
-    Displays a form users can fill out to edit an existing record.
-
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be edited.
-    """
-    doc = db.exampleapp.find_one({"_id": ObjectId(mongoid)})
-    return render_template(
-        "edit.html", mongoid=mongoid, doc=doc
-    )  # render the edit template
-
-
-@app.route("/edit/<mongoid>", methods=["POST"])
-def edit_post(mongoid):
-    """
-    Route for POST requests to the edit page.
-    Accepts the form submission data for the specified document and updates the document in the database.
-
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be edited.
-    """
-    name = request.form["fname"]
-    message = request.form["fmessage"]
-
-    doc = {
-        # "_id": ObjectId(mongoid),
+    booking_data = {
         "name": name,
-        "message": message,
-        "created_at": datetime.datetime.utcnow(),
+        "email": email,
+        "check_in_date": check_in_date,
+        "check_out_date": check_out_date,
+        "hotel_id": ObjectId(hotel_id),
+        "status": "checked_in"
     }
 
-    db.exampleapp.update_one(
-        {"_id": ObjectId(mongoid)}, {"$set": doc}  # match criteria
-    )
+    db.bookings.insert_one(booking_data)
 
-    return redirect(
-        url_for("read")
-    )  # tell the browser to make a request for the /read route
+    return redirect(url_for("booking_details", hotel_id=hotel_id))
 
+@app.route("/check_out/<booking_id>")
+def check_out(booking_id):
+    db.bookings.update_one({"_id": ObjectId(booking_id)}, {"$set": {"status": "checked_out"}})
+    return redirect(url_for("booking_details", booking_id=booking_id))
 
-@app.route("/delete/<mongoid>")
-def delete(mongoid):
-    """
-    Route for GET requests to the delete page.
-    Deletes the specified record from the database, and then redirects the browser to the read page.
+@app.route("/change_reservation/<booking_id>", methods=["POST"])
+def change_reservation(booking_id):
+    check_in_date = datetime.datetime.strptime(request.form["check_in_date"], "%Y-%m-%d")
+    check_out_date = datetime.datetime.strptime(request.form["check_out_date"], "%Y-%m-%d")
 
-    Parameters:
-    mongoid (str): The MongoDB ObjectId of the record to be deleted.
-    """
-    db.exampleapp.delete_one({"_id": ObjectId(mongoid)})
-    return redirect(
-        url_for("read")
-    )  # tell the web browser to make a request for the /read route.
+    db.bookings.update_one({"_id": ObjectId(booking_id)}, {"$set": {"check_in_date": check_in_date, "check_out_date": check_out_date}})
+    return redirect(url_for("booking_details", booking_id=booking_id))
 
+@app.route("/booking_details/<booking_id>")
+def booking_details(booking_id):
+    booking = db.bookings.find_one({"_id": ObjectId(booking_id)})
+    return render_template("booking_details.html", booking=booking)
 
-@app.route("/webhook", methods=["POST"])
-def webhook():
-    """
-    GitHub can be configured such that each time a push is made to a repository, GitHub will make a request to a particular web URL... this is called a webhook.
-    This function is set up such that if the /webhook route is requested, Python will execute a git pull command from the command line to update this app's codebase.
-    You will need to configure your own repository to have a webhook that requests this route in GitHub's settings.
-    Note that this webhook does do any verification that the request is coming from GitHub... this should be added in a production environment.
-    """
-    # run a git pull command
-    process = subprocess.Popen(["git", "pull"], stdout=subprocess.PIPE)
-    pull_output = process.communicate()[0]
-    # pull_output = str(pull_output).strip() # remove whitespace
-    process = subprocess.Popen(["chmod", "a+x", "flask.cgi"], stdout=subprocess.PIPE)
-    chmod_output = process.communicate()[0]
-    # send a success response
-    response = make_response(f"output: {pull_output}", 200)
-    response.mimetype = "text/plain"
-    return response
+@app.route("/cancel_booking/<booking_id>")
+def cancel_booking(booking_id):
+    db.bookings.delete_one({"_id": ObjectId(booking_id)})
+    return redirect(url_for("home"))
 
-
-@app.errorhandler(Exception)
-def handle_error(e):
-    """
-    Output any errors - good for debugging.
-    """
-    return render_template("error.html", error=e)  # render the edit template
-
-
-# run the app
 if __name__ == "__main__":
-    # logging.basicConfig(filename="./flask_error.log", level=logging.DEBUG)
-    app.run(load_dotenv=True)
+    app.run(debug=True)
